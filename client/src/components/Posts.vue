d8d9f9f423f8e7a0e2b51119dd388b971db66fb7
Garyc <garyc2232@gmail.com>
2018-09-03 18:11:14 +0800

    Implement SCSS style

--- client/src/components/Posts.vue
+++ client/src/components/Posts.vue
@@ -52,38 +52,43 @@ export default {
   }
 }
 </script>
-<style type="text/css">
+<style lang="scss" scoped>
+
 .table-wrap {
   width: 60%;
   margin: 0 auto;
   text-align: center;
 }
-table th, table tr {
-  text-align: left;
+table{
+  th, tr{
+    text-align: left;
+  }
+  thead {
+    background: #f2f2f2;
+  }
+  tr td {
+    padding: 10px;
+  }
+  tr:nth-child(odd) {
+    background: #f2f2f2;
+  }
+  tr:nth-child(1) {
+    background: #4d7ef7;
+    color: #fff;
+  }
 }
-table thead {
-  background: #f2f2f2;
-}
-table tr td {
-  padding: 10px;
-}
-table tr:nth-child(odd) {
-  background: #f2f2f2;
-}
-table tr:nth-child(1) {
-  background: #4d7ef7;
-  color: #fff;
-}
+
 a {
   color: #4d7ef7;
   text-decoration: none;
+  .add_post_link {
+    background: #4d7ef7;
+    color: #fff;
+    padding: 10px 80px;
+    text-transform: uppercase;
+    font-size: 12px;
+    font-weight: bold;
+  }
 }
-a.add_post_link {
-  background: #4d7ef7;
-  color: #fff;
-  padding: 10px 80px;
-  text-transform: uppercase;
-  font-size: 12px;
-  font-weight: bold;
-}
+
 </style>
